35f0cb0bd8d3f297aadef231ec6e00ebd7f238eb
MD Pahlovi <mdpahlovi07@gmail.com>
2023-09-05 01:05:48 +0600

    Project Setup

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,35 @@
 import { Server } from "http";
-import mongoose from "mongoose";
 import app from "./app";
 import config from "./config";
 
-process.on("uncaughtException", error => {
-    console.error(error);
-    process.exit(1);
-});
-
-let server: Server;
-
 async function bootstrap() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log(`Database is connected successfully`);
+    const server: Server = app.listen(config.port, () => {
+        console.log(`Server running on port ${config.port}`);
+    });
 
-        server = app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`);
-        });
-    } catch (err) {
-        console.error("Failed to connect database", err);
-    }
-
-    process.on("unhandledRejection", error => {
+    const exitHandler = () => {
         if (server) {
             server.close(() => {
-                console.error(error);
-                process.exit(1);
+                console.log("Server closed");
             });
-        } else {
-            process.exit(1);
+        }
+        process.exit(1);
+    };
+
+    const unexpectedErrorHandler = (error: unknown) => {
+        console.log(error);
+        exitHandler();
+    };
+
+    process.on("uncaughtException", unexpectedErrorHandler);
+    process.on("unhandledRejection", unexpectedErrorHandler);
+
+    process.on("SIGTERM", () => {
+        console.log("SIGTERM received");
+        if (server) {
+            server.close();
         }
     });
 }
 
 bootstrap();
-
-process.on("SIGTERM", () => {
-    console.log("SIGTERM is received");
-    if (server) {
-        server.close();
-    }
-});
